17c9fa3c84d2b77f5c525d030d7b4734acac323a
joy <joy@JoyPC.localdomain>
2021-04-28 19:34:27 +0530

    Deployment - Dockerfile & docker-compose added

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,8 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import 'firebase/firestore';
-
-const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  databaseURL: process.env.REACT_APP_databaseURL,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId
-});
+import firebaseConfig from './firebaseConfig';
+const app = firebase.initializeApp(firebaseConfig);
 
 export const auth = app.auth();
 export const db = firebase.firestore();
